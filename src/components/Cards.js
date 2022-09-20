68adbf2f1d9f3c43969f1cf3c6c1249765af8dd2
Cecilia Milla <98898104+ccmilla@users.noreply.github.com>
2022-09-20 11:28:14 -0400

    Add files via upload

---
+++ src/components/Cards.js
@@ -0,0 +1,12 @@
+function Cards(props){
+    return(
+      <div className="card">
+       <h2>{props.text}</h2>
+        <div className="actions">
+          
+          <button className="btn">Delete</button>
+        </div>
+      </div>
+    )
+}
+export default Cards;
